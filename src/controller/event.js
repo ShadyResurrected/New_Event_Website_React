890d622625d75634ca4267afa890e4029fdb6115
bornmay <mailhemantjoshi@gmail.com>
2022-05-28 09:48:12 +0530

    events fix

--- src/controller/event.js
+++ src/controller/event.js
@@ -29,6 +29,25 @@ exports.createNewEvent = async (req, res) => {
   return res.send(event);
 };
 
+// exports.editEvent = async (req,res) =>{
+//   const {eventId, isDelete, } = req.body;
+//   const {userId} = req.user;
+//   const event = await Event.findById({id: eventId}).exec();
+//   if(event.creator != userId){
+//     return res.status(400).json({
+//       body: "Error"
+//     })
+//   }
+//   else if(isDelete){
+//     await Event.deleteOne({_id: eventId}).exec();
+//     return res.status(400).json({
+//       body: "Deleted"
+//     })
+//   }else{
+
+//   }
+// }
+
 exports.getEvents = async (req, res) => {
   let event = await Event.find({}).exec();
   return res.send(event);
@@ -38,4 +57,13 @@ exports.getEventByID = async (req, res) => {
   let { eventId } = req.body;
   let eventById = await Event.findById(eventId);
   return res.send(eventById);
+};
+
+exports.getEventByUser = async (req, res) => {
+  const { email } = req.user;
+  const { _id } = await User.findOne({ email }).exec();
+  const events = await Event.find({ creator: _id });
+  return res.status(200).json({
+    events,
+  });
 };
--- src/routes/event.js
+++ src/routes/event.js
@@ -3,6 +3,7 @@ const {
   createNewEvent,
   getEvents,
   getEventByID,
+  getEventByUser,
 } = require('../controller/event');
 const { authCheck } = require('../middleware/auth');
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.post('/create-new-event', authCheck, createNewEvent);
 router.post('/getAllEvents', getEvents);
 router.post('/getEventById', getEventByID);
+router.post('/getEventByUser', authCheck, getEventByUser);
 
 module.exports = router;
