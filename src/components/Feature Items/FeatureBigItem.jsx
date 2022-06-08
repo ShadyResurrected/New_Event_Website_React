109995a83be1aac54f93266e92d8257507200f04
ShadyResurrected <cchandra554@gmail.com>
2022-06-08 10:50:03 +0530

    Corrected the paths of img tags

--- src/components/Feature Items/FeatureBigItem.jsx
+++ src/components/Feature Items/FeatureBigItem.jsx
@@ -9,7 +9,7 @@ const FeatureBigItem = () => {
                 <div class="container">
                     <div class="row">
                         <div class="col-lg-5 col-md-12 col-sm-12 align-self-center" data-scroll-reveal="enter left move 30px over 0.6s after 0.4s">
-                            <img src="\src\assests\images\left-image.png" class="rounded img-fluid d-block mx-auto" alt="App" />
+                            <img src="..\src\assests\images\left-image.png" class="rounded img-fluid d-block mx-auto" alt="App" />
                         </div>
                         <div class="col-lg-1"></div>
                         <div class="col-lg-6 col-md-12 col-sm-12 align-self-center mobile-top-fix">
@@ -46,7 +46,7 @@ const FeatureBigItem = () => {
                         </div>
                         <div class="col-lg-1"></div>
                         <div class="col-lg-5 col-md-12 col-sm-12 align-self-center mobile-bottom-fix-big" data-scroll-reveal="enter right move 30px over 0.6s after 0.4s">
-                            <img src="\src\assests\images\right-image.png" class="rounded img-fluid d-block mx-auto" alt="App" />
+                            <img src="..\src\assests\images\right-image.png" class="rounded img-fluid d-block mx-auto" alt="App" />
                         </div>
                     </div>
                 </div>
--- src/components/Feature Items/FeatureSmallItem.jsx
+++ src/components/Feature Items/FeatureSmallItem.jsx
@@ -12,7 +12,7 @@ const FeatureIt = () => {
                             <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.2s">
                                 <div class="glass-effect features-small-item">
                                     <div class="icon">
-                                        <i class="classlogo"><img src="\src\assests\images\logo1.png" alt="" /></i>
+                                        <i class="classlogo"><img src="..\src\assests\images\logo1.png" alt="" /></i>
                                     </div>
                                     <h5 class="features-title">Stats</h5>
                                     <p>Analytics at your fingertips! See how you have been performing over the entire course of your presence on the platform.
@@ -24,7 +24,7 @@ const FeatureIt = () => {
                             <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.4s">
                                 <div class="glass-effect features-small-item">
                                     <div class="icon">
-                                        <i class="classlogo"><img src="\src\assests\images\logo2.png" alt="" /></i>
+                                        <i class="classlogo"><img src="..\src\assests\images\logo2.png" alt="" /></i>
                                     </div>
                                     <h5 class="features-title">Earn Prizes</h5>
                                     <p>Winners will get monetary rewards: Who does not like winning some extra cash doing things they anyway love to do? Here at our platform, not only your creativity and skill are unleashed, but, you get rewarded as well.</p>
@@ -34,7 +34,7 @@ const FeatureIt = () => {
                             <div class="col-lg-4 col-md-6 col-sm-6 col-12" data-scroll-reveal="enter bottom move 50px over 0.6s after 0.6s">
                                 <div class="glass-effect features-small-item">
                                     <div class="icon">
-                                        <i class="classlogo"><img src="\src\assests\images\logo3.png" alt="" /></i>
+                                        <i class="classlogo"><img src="..\src\assests\images\logo3.png" alt="" /></i>
                                     </div>
                                     <h5 class="features-title">Fun Events</h5>
                                     <p>Everything this platform hosts is so fun to be part of, that you will lose track of time. You can dance, play football, compete in chess games, or be part of any community you like. 
