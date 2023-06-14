c30b81994c1cbcbd28951e79c12a0423150f5a2f
ShadyResurrected <cchandra554@gmail.com>
2023-06-14 14:05:49 +0530

    env variable client url
    Signed-off-by: ShadyResurrected <cchandra554@gmail.com>

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ export const app = express();
 app.use(express.json()); // accepting the json data from the body
 app.use(cookieParser()); // to access the stored cookie
 
-app.use(
-  cors({
-    origin: ["http://localhost:3000","https://new-event-website-react.vercel.app"], // website url that should be allowed to access this api
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+const corsOptions = {
+  origin: process.env.CLIENT_URL,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use("/users", userRouter);
